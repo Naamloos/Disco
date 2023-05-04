c89d00b845815a42183871734fae0bc5b5a1af07
Ryan de Jonge <ryandejonge@outlook.com>
2023-05-04 19:47:07 +0200

    update experiments script

--- Disco/Javascript/Experiments.js
+++ Disco/Javascript/Experiments.js
@@ -1,9 +1,15 @@
-﻿webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache = Object.values(r.c)]);
-var UserStore = cache.find(m => m?.exports?.default?.getCurrentUser).exports.default;
-var actions = UserStore._dispatcher._actionHandlers._orderedActionHandlers["CONNECTION_OPEN"];
-var user = UserStore.getCurrentUser();
-actions.find(n => n.name === "ExperimentStore").actionHandler({
-	type: "CONNECTION_OPEN", user: { flags: user.flags |= 1 }, experiments: [],
-});
-actions.find(n => n.name === "DeveloperExperimentStore").actionHandler();
-webpackChunkdiscord_app.pop(); user.flags &= ~1; "done";
+﻿
+
+let wpRequire;
+window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => { wpRequire = req; }]);
+mod = Object.values(wpRequire.c).find(x => typeof x?.exports?.Z?.isDeveloper !== "undefined");
+usermod = Object.values(wpRequire.c).find(x => x?.exports?.default?.getUsers)
+nodes = Object.values(mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes)
+try {
+    nodes.find(x => x.name == "ExperimentStore").actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } })
+} catch (e) { }
+oldGetUser = usermod.exports.default.__proto__.getCurrentUser;
+usermod.exports.default.__proto__.getCurrentUser = () => ({ isStaff: () => true })
+nodes.find(x => x.name == "DeveloperExperimentStore").actionHandler["CONNECTION_OPEN"]()
+usermod.exports.default.__proto__.getCurrentUser = oldGetUser
+
